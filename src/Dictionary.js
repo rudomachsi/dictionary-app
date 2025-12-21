399801004611e3b4f244b2d7bb442b30a00c2f56
Rudo Machisi <rudomachisi@gmail.com>
2025-12-21 03:50:39 +0200

    initial cmmir

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,39 +1,74 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import axios from "axios";
 import Results from "./Results";
+import Photos from "./Photos"
 import "./Dictionary.css";
 
+export default function Dictionary(props) {
+  let [keyword, setKeyword] = useState(props.defaultKeyword);
+  let [results, setResults] = useState(null);
+  let [loaded, setLoaded] = useState(false);
+  let [photos, setPhotos] = useState(null);
 
-export default function Dictionary() {
-let [keyword, setKeyword]= useState("");
-let [results, setResults]= useState(null);
+  function handleDictionaryResponse(response) {
+    setResults(response.data[0]);
+  }
 
+  function handlePexelsResponse(response) {
+    console.log(response);
+    setPhotos(response.data.photos);
+  }
 
-function handleResponse(response) {
+  function search() {
+    // Dictionary API documentation: https://api.dictionaryapi.dev/
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
+    console.log(apiUrl);
+    axios.get(apiUrl).then(handleDictionaryResponse);
+
+    // Pexels API
+    let pexelsApiKey = "HovLdX5meDFiZIjVI9HJrRDonlw4fcxyByGJHchC2qXkXfK8hsXCsMYW";
+    let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=9`;
+    let headers = { Authorization:  `${pexelsApiKey}` };
     
-     setResults(response.data[0]);
-}
-     function search(event){
-        event.preventDefault();
-     alert( `Searching for ${keyword} definition`);
+    axios.get(pexelsApiUrl, { headers: headers }).then(handlePexelsResponse);
+  }
 
-     //documentation: https://api.dictionaryapi.dev/
-     let apiUrl= `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
-     console.log(apiUrl);
-     axios.get(apiUrl).then(handleResponse);
-}
+  function handleSubmit(event) {
+    event.preventDefault();
+    search();
+  }
 
-
-function handleKeywordChange(event){
+  function handleKeywordChange(event) {
     setKeyword(event.target.value);
-}
+  }
 
+  function load() {
+    setLoaded(true);
+    search();
+  }
+
+  if (loaded) {
     return (
-    <div className="Dictionary">
-        <form onSubmit={search}>
-            <input type="search"onChange={handleKeywordChange}/>
-        </form>
-        <Results results={results}/>
-    </div>
+      <div className="Dictionary">
+        <section>
+          <h1>What word do you want to look up?</h1>
+          <form onSubmit={handleSubmit}>
+            <input
+              type="search"
+              onChange={handleKeywordChange}
+              defaultValue={props.defaultKeyword}
+            />
+          </form>
+          <div className="hint">
+            suggested words: sunset, sunrise, wine, yoga, beautiful, flabbergasted.....
+          </div>
+        </section>
+        <Results results={results}/> 
+        <Photos photo={photos} />
+      </div>
     );
+  } else {
+    load();
+    return "loading";
+  }
 }
--- src/Phonetic.js
+++ src/Phonetic.js
@@ -1,14 +1,17 @@
-import react from "react";
+//import react from "react";
+import "./Phonetic.css";
 
-export default function Phonetic(props){
-    console.log(props.phonetic);
-    return (
-        <div className="Phonetic">
-            <a href={props.phonetic.audio} target="_blank">
-                Listen
-            </a>
-            <br/>
-            {props.phonetic.text}
-        </div>
-    );
+export default function Phonetic(props) {
+ 
+  return (
+
+    <div className="Phonetic">
+   
+      <a href={props.phonetic.audio} target="_blank" rel="noreferrer">
+        Listen
+      </a>
+      <span className="text">{props.phonetic.text}</span>
+      {props.phonetic.text}
+    </div>
+  );
 }
